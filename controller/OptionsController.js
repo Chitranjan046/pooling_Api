998e2041344215f8b1e53fb6f23ab73c14cfde66
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-07-08 12:16:17 +0530

    second

--- controller/OptionsController.js
+++ controller/OptionsController.js
@@ -2,7 +2,7 @@ const Option = require('../models/options');
 const Question = require('../models/questions');
 
 // Create an option for a given question ID
-exports.create = async (req, res) => {
+module.exports.create = async function (req, res) {
     // Create options for the specified question ID
     console.log(req.body, req.params.id);
     const opt = await Option.create({
@@ -14,43 +14,43 @@ exports.create = async (req, res) => {
     const updateOpt = await Option.findByIdAndUpdate(opt._id, { "add_vote": `http://localhost:8080/api/v1/options/${opt._id}/add_vote` });
     updateOpt.save();
 
-    const ques = await Question.findById(req.params.id);
-    if (ques) {
-        ques.options.push(updateOpt);
-        ques.save();
-        console.log(ques);
-        res.send(ques);
+    const question = await Question.findById(req.params.id);
+    if (question) {
+        question.options.push(updateOpt);
+        question.save();
+        console.log(question);
+        res.send(question);
     } else {
         res.send('Question does not exist');
     }
-};
+}
 
 // Add a vote to a specific option
-exports.add_vote = async (req, res) => {
+module.exports.add_vote = async function (req, res) {
     // Add a vote to the specified option
     console.log(req.params.id);
-    const opt = await Option.findByIdAndUpdate(req.params.id, { $inc: { vote: 1 } });
-    if (opt) {
-        await opt.save();
-        console.log(opt);
-        res.send(opt);
+    const option= await Option.findByIdAndUpdate(req.params.id, { $inc: { vote: 1 } });
+    if (option) {
+        await opttion.save();
+        console.log(option);
+        res.send(option);
     } else {
         res.send('Option does not exist');
     }
-};
+}
 
 // Delete a specific option
-exports.delete = async (req, res) => {
+module.exports.delete = async function (req, res) {
     // Delete the specified option
     console.log('ID:', req.params.id);
-    const opt = await Option.findById(req.params.id);
-    if (opt) {
-        const quesId = opt.question;
-        const ques = await Question.findByIdAndUpdate(quesId, { $pull: { options: req.params.id } });
+    const option = await Option.findById(req.params.id);
+    if (option) {
+        const questionId = opt.question;
+        const question = await Question.findByIdAndUpdate(questionId, { $pull: { options: req.params.id } });
         await Option.findByIdAndDelete(req.params.id);
-        console.log(ques);
+        console.log(question);
         res.send('Option deleted');
     } else {
         res.send('ID does not exist');
     }
-};
+}
